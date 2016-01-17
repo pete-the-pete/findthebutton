bd042208779f99e21ae516999265b1b014f2822c
pete_the_pete <pliljegr@gmail.com>
2016-01-17 12:46:49 -0800

    Adding basic routes

--- app/router.js
+++ app/router.js
@@ -6,6 +6,10 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('about');
+  this.route('leaderboard');
+  this.route('game');
+  this.route('home');
 });
 
 export default Router;
