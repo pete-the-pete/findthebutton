1b0c7f87cbc938f2ef50d1ef223b8252285d0f49
pete_the_pete <pliljegr@gmail.com>
2016-01-21 20:36:33 -0800

    Moving logic into TLC b/c that is where it belongs.

--- app/components/game-container.js
+++ app/components/game-container.js
@@ -1,4 +1,93 @@
 import Ember from 'ember';
 
+const LEVELS = [
+  'Peek-a-Boo'
+];
+
+/**
+ * TLC That operates like a controller, there is very little
+ * state that comes from the route, it mainly get sent
+ * to save the score.
+ */
 export default Ember.Component.extend({
+
+  /**
+   * Unecessary comment.
+   */
+  currentLevel: 0,
+
+  /**
+   * Total time actually 'playing'
+   */
+  playTime: 0,
+
+  /**
+   * Signifies that the user has actually started playing a level,
+   * to distinguish from isPlaying
+   */
+  hasStarted: false,
+
+  /**
+   * Signifies when the user is actually playing, so the timer
+   * will be running and count towards their score.
+   */
+  isPlaying: false,
+
+  /**
+   * One day...
+   */
+  isWinner: false,
+
+  /**
+   * glob of level data that updates based on the current level.
+   */
+  level: Ember.computed('currentLevel', function() {
+    let levelTitle = LEVELS[this.get('currentLevel')];
+    return {
+      levelTitle,
+      currentLevel: this.get('currentLevel'),
+      levelFile: levelTitle.toLowerCase()
+    };
+  }),
+
+  _start() {
+    this.set('hasStarted', true);
+    this.set('isPlaying', true);
+  },
+
+  _stop() {
+    this.set('hasStarted', false);
+    this.set('isPlaying', false);
+  },
+
+  _pause() {
+    this.set('isPlaying', false);
+  },
+
+  actions: {
+    startPlaying() {
+      this._start();
+    },
+
+    pausePlaying() {
+      this._pause();
+    },
+
+    advanceLevel() {
+      this._stop();
+      this.incrementProperty('currentLevel');
+      if(this.get('currentLevel') === LEVELS.length) {
+        this.set('isWinner', true);
+      }
+    },
+
+    resetGame() {
+      this.setProperties({
+        'hasStarted': false,
+        'isPlaying': false,
+        'currentLevel': 0,
+        'playTime': 0
+      });
+    }
+  }
 });
--- app/routes/game.js
+++ app/routes/game.js
@@ -1,98 +1,21 @@
 import Ember from 'ember';
 
-const LEVELS = [
-  'Peek-a-Boo'
-];
-
+/**
+ * Doesn't do much, let's the TLC know if the user
+ * is logged in, and saves results.
+ */
 export default Ember.Route.extend({
-  /**
-   * Unecessary comment.
-   */
-  currentLevel: 0,
 
   /**
-   * Total time actually 'playing'
-   */
-  playTime: 0,
-
-  /**
-   * Signifies when the user is actually playing, so the timer
-   * will be running and count towards their score.
-   */
-  isPlaying: false,
-
-  /**
-   * One day...
-   */
-  isWinner: false,
-
-  /**
-   * glob of level data that updates based on the current level.
-   */
-  level: Ember.computed('currentLevel', function() {
-    let levelTitle = LEVELS[this.get('currentLevel')];
-    return {
-      levelTitle,
-      currentLevel: this.get('currentLevel'),
-      levelFile: levelTitle.toLowerCase()
-    };
-  }),
-
-  /**
-   * Pass the current state to the templates
+   * Pass along the session data.
    */
   model() {
-    let model = this.getProperties(
-      'level',
-      'playTime',
-      'isWinner',
-      'isPlaying'
-    );
-    model.LEVELS = LEVELS;
-    return model;
-  },
-
-  _start() {
-    this.set('isPlaying', true);
-    this.set('currentModel.isPlaying', this.get('isPlaying'));
-  },
-
-  _pause() {
-    this.set('isPlaying', false);
-    this.set('currentModel.isPlaying', this.get('isPlaying'));
-  },
-
-  /**
-   * Resets everything back to 0
-   * @private
-   */
-  _reset() {
-    this.setProperties({
-      'isPlaying': false,
-      'currentLevel': 0,
-      'playTime': 0
-    });
+    return this.get('session').isAuthenticated;
   },
 
   actions: {
-    startPlaying() {
-      this._start();
-    },
-
-    pausePlaying() {
-      this._pause();
-    },
-
     advanceLevel() {
-      this.incrementProperty('currentLevel');
-      if(this.get('currentLevel') === LEVELS.length) {
-        this.set('currentModel.isWinner', true);
-        this._pause();
-      }
-    },
-
-    resetGame() {
-      this._reset();
+      //save some data
     }
   }
 });
