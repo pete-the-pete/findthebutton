f59968d5c73a92c751558cdb0b41f32723d662ad
pete_the_pete <pliljegr@gmail.com>
2016-02-26 21:27:33 -0800

    Hotcold positioning and scrolling correctly

--- app/components/levels/hot-cold.js
+++ app/components/levels/hot-cold.js
@@ -13,48 +13,39 @@ export default BaseLevel.extend({
     if(this.initialRender) {
       this.initialRender = false;
 
-      let el = this.get('element');
-      let lastDimensions = el.getBoundingClientRect();
-      let currentLevel = this.get('currentLevel');
-
       Ember.run(() => {
-        let dimensions = currentLevel > 0 ? (100 * (1+(currentLevel/10))) : 100;
+        let el = this.get('element');
+        let lastDimensions = el.getBoundingClientRect();
+        let currentLevel = this.get('currentLevel');
+        let {height: bHeight, width: bWidth} = this.buttonRect;
+        let dimensions = currentLevel > 0 ? (100 * (1+(currentLevel/2))) : 100;
         el.style.width = `${dimensions}%`;
         el.style.height = `${dimensions}%`;
 
         let midPoint = lastDimensions.width/2;
         let multiplier = dimensions/100;
         let excludeRange = [
-          midPoint - (multiplier * this.buttonRect.width),
-          midPoint + (multiplier * this.buttonRect.width)
+          midPoint - (multiplier * bWidth),
+          midPoint + (multiplier * bWidth)
         ];
         let maximums = {
-          height: (lastDimensions.height * multiplier) - this.buttonRect.height,
-          width: (lastDimensions.width * multiplier) - this.buttonRect.width
+          height: (lastDimensions.height * multiplier) - bHeight,
+          width: (lastDimensions.width * multiplier) - bWidth
         };
 
         let heatMap = this.get('element').querySelector('#heat-map');
         let computedStyle = window.getComputedStyle(heatMap);
-        this._positionButton(maximums, this.buttonRect.width, excludeRange).then((buttonPos) => {
+        this._positionButton(maximums, bWidth, excludeRange).then((buttonPos) => {
           Ember.run(() => {
-            let yCenter = buttonPos.top + this.buttonRect.height/2;
-            let xCenter = buttonPos.left + this.buttonRect.width/2;
+            let yCenter = buttonPos.top + bHeight/2;
+            let xCenter = buttonPos.left + bWidth/2;
             let newBackground = computedStyle.background.replace(/circle at ([^,]*),/, `circle at ${xCenter}px ${yCenter}px,`);
             heatMap.style.background = newBackground;
           });
         });
+        el.parentNode.scrollTop = Math.floor(el.scrollHeight/2);
+        el.parentNode.scrollLeft = Math.floor(el.scrollWidth/2);
       });
-
-      // if(this.initialRender) {
-      //   this.initialRender = false;
-
-      //   let el = this.get('element');
-      //   //scroll to the center of the level
-      //   Ember.run.next(() => {
-      //     el.parentNode.scrollTop = Math.floor(el.scrollHeight/2);
-      //     el.parentNode.scrollLeft = Math.floor(el.scrollWidth/2);
-      //   });
-      // }
     }
   },
 
