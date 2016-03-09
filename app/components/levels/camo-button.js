fdb0c4d1bb664f58676b3d6836bb8e4730eac87e
pete_the_pete <pliljegr@gmail.com>
2016-03-08 22:33:27 -0800

    Using background images

--- app/components/levels/camo-button.js
+++ app/components/levels/camo-button.js
@@ -5,13 +5,13 @@ export default BaseLevel.extend({
   classNames: ['camo-button'],
   classNameBindings: ['camoLevel'],
 
-  init() {
-    this._super(...arguments);
-    this.set('camoLevel', `camo-${Math.floor(this.get('attrs.currentLevel.value') / 5) + 1}`);
-  },
-
   didRender() {
     this._super(...arguments);
+
+    let val = this.get('attrs.currentLevel.value');
+    let level = val > 0 ? val : 1;
+
+    this.set('camoLevel', `camo-${level % 5}`);
     this._positionButton(this.get('element').getBoundingClientRect());
   }
 });
